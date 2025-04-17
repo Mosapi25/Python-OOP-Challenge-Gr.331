ebb7b39c8f6e330788504fbb91bdd51379ab9265
Mosa Mokhaneli <dikeledi.mokhaneli@gmail.com>
2025-04-17 21:39:58 +0200

    Digital Pet

---
+++ main.py
@@ -0,0 +1,13 @@
+from pet import Pet
+
+my_pet = Pet("Buddy")
+
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("Roll over")
+my_pet.train("Sit")
+
+my_pet.get_status()
+my_pet.show_tricks()
